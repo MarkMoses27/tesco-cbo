9a23ad810d3b59009da6cbe8cdd51e4e444ebbd9
MarkMoses27 <markmoses817@gmail.com>
2025-03-03 18:15:34 +0300

    resized

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
-            if (id.includes('lodash')) return 'lodash-vendor';
-            return 'vendor'; // Other dependencies
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
-    brotliSize: true, // Enable Brotli compression
-  },
-});
+  plugins: [   
+    tailwindcss(),
+    react()],
+})
+
